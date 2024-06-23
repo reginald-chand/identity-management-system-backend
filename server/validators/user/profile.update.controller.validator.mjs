d846b08b124b2cac68736526c3b512c0be3bcfcc
Reginald Chand <reginald-chand@outlook.com>
2024-06-23 21:39:26 +1200

    Create profile.update.controller.validator.mjs

---
+++ server/validators/user/profile.update.controller.validator.mjs
@@ -0,0 +1,29 @@
+import Joi from "joi";
+
+export const profileUpdateControllerValidator = Joi.object({
+  _csrf: Joi.string().required(),
+
+  firstName: Joi.string().pattern(new RegExp("^[A-Z].*$")),
+  middleName: Joi.string().pattern(new RegExp("^[A-Z].*$")),
+  lastName: Joi.string().pattern(new RegExp("^[A-Z].*$")),
+  userName: Joi.string().pattern(new RegExp("^[a-z]+$")),
+
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+
+  phone: Joi.string().pattern(new RegExp("^(d{2,3})sd{3}sd+$")),
+  dateOfBirth: Joi.string().pattern(
+    new RegExp("^(0[1-9]|[12][0-9]|3[01])\\/(0[1-9]|1[012])\\/(19|20)\\d\\d$")
+  ),
+
+  age: Joi.number(),
+  gender: Joi.string().pattern(new RegExp("^[A-Z][a-z]*(?: [A-Z][a-z]*)?$")),
+  occupation: Joi.string().pattern(
+    new RegExp("^[A-Z][a-z]*(?: [A-Z][a-z]*)?$")
+  ),
+
+  country: Joi.string().pattern(new RegExp("^[A-Z][a-z]*(?: [A-Z][a-z]*)?$")),
+  user: Joi.object().required(),
+});
