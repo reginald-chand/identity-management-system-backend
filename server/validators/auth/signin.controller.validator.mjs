dddf5a1de8d3b7f440303a4e40242b3ed284a37d
Reginald Chand <reginald-chand@outlook.com>
2024-06-23 21:38:47 +1200

    Create signin.controller.validator.mjs

---
+++ server/validators/auth/signin.controller.validator.mjs
@@ -0,0 +1,20 @@
+import Joi from "joi";
+
+export const signInControllerValidator = Joi.object({
+  _csrf: Joi.string().required(),
+
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+
+  password: Joi.string()
+    .pattern(
+      new RegExp(
+        "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
+      )
+    )
+    .required(),
+});
