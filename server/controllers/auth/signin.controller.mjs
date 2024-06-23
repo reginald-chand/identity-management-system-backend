011b8b02b94a6a4405d5b82ef0a787e200558db1
Reginald Chand <reginald-chand@outlook.com>
2024-06-23 21:41:38 +1200

    Create signin.controller.mjs

---
+++ server/controllers/auth/signin.controller.mjs
@@ -0,0 +1,83 @@
+import { AccessTokenModel } from "../../models/user/access.token.blacklist.model.mjs";
+import { SignUpModel } from "../../models/auth/signup.model.mjs";
+import bcrypt from "bcryptjs";
+import { cookieOptions } from "../../configs/cookie.options.config.mjs";
+import jwt from "jsonwebtoken";
+import { logger } from "../../configs/logger.config.mjs";
+import { signInControllerValidator } from "../../validators/auth/signin.controller.validator.mjs";
+
+export const signInController = async (request, response) => {
+  const { error, value } = signInControllerValidator.validate(request.body);
+
+  if (error) {
+    return response.status(400).json({ responseMessage: error.message });
+  }
+
+  const { email, password } = value;
+
+  try {
+    const existingUser = await SignUpModel.findOne({ email: { $eq: email } });
+
+    if (!existingUser) {
+      return response
+        .status(401)
+        .json({ responseMessage: "Email or Password is incorrect." });
+    }
+
+    const matchPasswordHash = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!matchPasswordHash) {
+      return response
+        .status(401)
+        .json({ responseMessage: "Email or Password is incorrect." });
+    }
+
+    const userInformation = {
+      id: existingUser._id,
+      firstName: existingUser.firstName,
+      lastName: existingUser.lastName,
+      email: existingUser.email,
+    };
+
+    const accessToken = jwt.sign(userInformation, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    if (!accessToken) {
+      return response.status(404).json({ responseMessage: "Token not found." });
+    }
+
+    const existingAccessTokens = await AccessTokenModel.exists({
+      accessTokens: { $eq: accessToken },
+    });
+
+    if (existingAccessTokens) {
+      return response.status(403).json({ responseMessage: "Cannot login." });
+    }
+
+    const refreshToken = jwt.sign(userInformation, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    return response
+      .status(200)
+      .header("Authorization", accessToken)
+      .cookie("refreshToken", refreshToken, cookieOptions)
+      .json({
+        responseMessage: `Login Successful. Welcome ${existingUser.firstName} ${existingUser.lastName}`,
+      });
+  } catch (error) {
+    logger.log({
+      level: "error",
+      message: error,
+      additional: "Internal server error.",
+    });
+
+    return response.status(500).json({
+      responseMessage: "Internal server error.",
+    });
+  }
+};
