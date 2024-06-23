fde3287a4efda74ade19787f7c33164e786f5948
Reginald Chand <reginald-chand@outlook.com>
2024-06-23 21:36:35 +1200

    Create signup.model.mjs

---
+++ server/models/auth/signup.model.mjs
@@ -0,0 +1,31 @@
+import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
+import slugify from "slugify";
+
+const signUpSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  middleName: { type: String },
+  lastName: { type: String, required: true },
+  userName: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  confirmPassword: { type: String },
+  isUserVerified: { type: Boolean, required: true },
+});
+
+signUpSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  try {
+    this.userName = slugify(this.userName, { lower: true, strict: true });
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+export const SignUpModel = mongoose.model("User", signUpSchema);
