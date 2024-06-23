c8ad69200e688423bd2a7d7655f508d0708a5bd4
Reginald Chand <reginald-chand@outlook.com>
2024-06-23 21:38:24 +1200

    Create signup.controller.validator.mjs

---
+++ server/validators/auth/signup.controller.validator.mjs
@@ -0,0 +1,27 @@
+import Joi from "joi";
+
+export const signUpControllerValidator = Joi.object({
+  _csrf: Joi.string().required(),
+
+  firstName: Joi.string().pattern(new RegExp("^[A-Z].*$")).required(),
+  middleName: Joi.string().pattern(new RegExp("^[A-Z].*$")),
+  lastName: Joi.string().pattern(new RegExp("^[A-Z].*$")).required(),
+  userName: Joi.string().pattern(new RegExp("^[a-z]+$")).required(),
+
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+
+  password: Joi.string()
+    .pattern(
+      new RegExp(
+        "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
+      )
+    )
+    .required(),
+
+  confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
+});
